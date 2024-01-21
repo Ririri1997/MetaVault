7ac47bd33c938128a3a26b632236046bff4599ff
Ririri1997 <ircher1997@gmail.com>
2024-01-21 23:27:47 +0300

    add all

--- js/main.js
+++ js/main.js
@@ -1,54 +1,16 @@
 'use strict'
-
-//theme
-const headerMode = document.querySelector('.header-mode');
+window.addEventListener('resize', mobile)
 
 
-headerMode.addEventListener('click', function() {
-  if (!document.body.hasAttribute('light')) {
-    document.body.setAttribute('light', '');
-  } else {
-    document.body.removeAttribute('light');
-  }
-});
-//  Переход по якорным ссылкам
-const anchors = document.querySelectorAll(".scroll-to");
-const menuItems = document.querySelectorAll(".link-nav");
+const dataAttr = document.querySelectorAll('[data-wow-delay]');
 
-anchors.forEach((link) => {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const href = this.getAttribute("href").substring(1);
-    const scrollTarget = document.getElementById(href);
-    const topOffset = 120;
-    const elementPosition = scrollTarget.getBoundingClientRect().top;
-    const offsetPosition = elementPosition - topOffset;
-
-    window.scrollBy({
-      top: offsetPosition,
-      behavior: "smooth",
+function mobile(){
+  if( window.innerWidth <= 680 ){
+    dataAttr.forEach(function(element) {
+      element.removeAttribute('data-wow-delay');
     });
-  });
-});
-
-// переход по якорным ссылкам из мобильного меню
-menuItems.forEach((item) => {
-  item.addEventListener("click", () => {
-   toggleMenu();
-   body.classList.remove('no-scroll');
-  });
-});
-
-//slider
-const swiper = new Swiper('.swiper', {
-  loop: true,
-  slidesPerView: 'auto',
-  spaceBetween: 30,
-  autoplay: {
-    delay: 2000,
-  },
-
-});
-
+ }
+}
+mobile();
 //animate 
 new WOW().init();
